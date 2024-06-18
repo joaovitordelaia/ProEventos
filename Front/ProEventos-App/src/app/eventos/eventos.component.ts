0ad763f6650a1e6c25f76f9d991bad37f28bad3f
João Vitor Delaia <joaovdelaia@gmail.com>
2024-06-18 20:37:00 -0300

    Seção 5: Angular, Interpolação, • Diretiva e Binding

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,7 +9,34 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
 
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  imageSize = "150";
+  imageMargin = "1";
+  isCollapsed = true;
+  mostrarImagem: boolean = true;
+  private _filtroLista : string = '';
+
+
+  public get filtroLista() : string {
+    return this._filtroLista;
+  }
+
+
+  public set filtroLista(value : string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+
+  }
+
+  filtrarEventos(filtrarPor : string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: { tema: string; local: string; }) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
+
 
 
   constructor(private http: HttpClient) { }
@@ -20,12 +47,17 @@ export class EventosComponent implements OnInit {
 
   public getEventos() : void {
       this.http.get('https://localhost:5001/api/eventos').subscribe(
-        response => this.eventos = response,
+        response => {
+          this.eventos = response
+          this.eventosFiltrados = response
+        },
         error => console.log(error)
         //complete
       );
   }
-
-
-
+  public onHeaderClick(){ /*(click)= "onHeaderClick()">*/
+      this.mostrarImagem = !this.mostrarImagem
+  }
 }
+
+/*class="btn btn-outline-light btn-sm">*/
